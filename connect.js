0e5f103a3c57b147fb942cbee46f922997618bfa
24s174-arch <24s174@yongsan.sen.hs.kr>
2026-05-29 08:23:10 +0900

    Update connect.js

--- connect.js
+++ connect.js
@@ -18,7 +18,7 @@ const path = require('path');
 app.get('/', (req, res) => {
     // __dirname은 현재 app.js가 있는 폴더 위치입니다.
     // 여기서는 app.js와 같은 위치에 있는 index.html을 보낸다고 가정합니다.
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join('index.html'));
 });
 
 app.get('/read-file', (req, res) => {
